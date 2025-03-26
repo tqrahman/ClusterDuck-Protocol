e928aac801326cc33dd48bf9ac319a9a28f1570f
Taraqur <tqrahman8@gmail.com>
2025-03-26 14:00:36 -0400

    fixed issue
    Signed-off-by: Taraqur <tqrahman8@gmail.com>

--- src/Ducks/MamaDuck.cpp
+++ src/Ducks/MamaDuck.cpp
@@ -120,8 +120,8 @@ void MamaDuck::handleReceivedPacket() {
 
             additional_data.insert(additional_data.end(), rssiString.begin(), rssiString.end());
             additional_data.insert(additional_data.end(), snrString.begin(), snrString.end());
+            rxPacket->addToBuffer(additional_data);
           }
-          rxPacket->addToBuffer(additional_data);
         }
         default:
           err = duckRadio.relayPacket(rxPacket);
